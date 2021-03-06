a342968ecd97332a70fd7dd100fc99c3643c78b1
ali raza <mraliryu@gmail.com>
2021-03-06 18:02:13 +0500

    Added humidity bar in homepage

---
+++ lib/custom_widgets/humidity_bar.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
+import 'package:klime/components/app_colors.dart';
+
+final GlobalKey<AnimatedCircularChartState> _chartKey =
+    new GlobalKey<AnimatedCircularChartState>();
+
+class HumidityBar extends StatefulWidget {
+  final double currentHumidity;
+
+  HumidityBar({this.currentHumidity});
+
+  @override
+  _HumidityBarState createState() => _HumidityBarState();
+}
+
+class _HumidityBarState extends State<HumidityBar> {
+  double currentBarValue;
+  double subtotalBarValue;
+  int holeLabel;
+
+  @override
+  void initState() {
+    super.initState();
+    updateBarValues(widget.currentHumidity);
+  }
+
+  void updateBarValues(double currentHumidity) {
+    currentBarValue = currentHumidity;
+    subtotalBarValue = 100 - currentHumidity;
+    holeLabel = currentHumidity.toInt();
+    print(holeLabel);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(height: 15.0),
+        Text(
+          'Humidity',
+          style: TextStyle(
+            height: 0.0,
+            color: AppColors.white,
+          ),
+        ),
+        SizedBox(height: 2.0),
+        AnimatedCircularChart(
+          key: _chartKey,
+          size: Size(130.0, 130.0),
+          initialChartData: <CircularStackEntry>[
+            CircularStackEntry(
+              <CircularSegmentEntry>[
+                CircularSegmentEntry(
+                  currentBarValue,
+                  AppColors.white,
+                  rankKey: 'current',
+                ),
+                CircularSegmentEntry(
+                  subtotalBarValue,
+                  AppColors.white.withOpacity(0.5),
+                  rankKey: 'subTotal',
+                ),
+                CircularSegmentEntry(
+                  20,
+                  AppColors.darkGrey,
+                  rankKey: 'empty',
+                ),
+              ],
+            ),
+          ],
+          startAngle: 120,
+          holeRadius: 42.0,
+          chartType: CircularChartType.Radial,
+          holeLabel: '$holeLabel %',
+          labelStyle: TextStyle(
+            color: AppColors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.w200,
+          ),
+          edgeStyle: SegmentEdgeStyle.round,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                height: 0.0,
+                color: AppColors.white.withOpacity(0.5),
+                fontSize: 12.0,
+              ),
+            ),
+            const SizedBox(width: 24.0),
+            Text(
+              '100',
+              style: TextStyle(
+                height: 0.0,
+                color: AppColors.white.withOpacity(0.5),
+                fontSize: 12.0,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:klime/components/app_colors.dart';
+import 'package:klime/custom_widgets/humidity_bar.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -8,144 +9,145 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.darkGrey,
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(bottom: 15.0),
-                child: Column(
-                  children: [
-                    const Expanded(child: SizedBox(height: 10.0)),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '30',
-                          style: TextStyle(
-                            fontSize: 56,
-                            color: AppColors.white,
-                            fontWeight: FontWeight.w100,
-                          ),
-                        ),
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(bottom: 28.0, left: 5.0),
-                          child: Text(
-                            '\u00B0',
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 15.0),
+                  child: Column(
+                    children: [
+                      const Expanded(child: SizedBox(height: 10.0)),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '21',
                             style: TextStyle(
-                              // height: 0.0,
-                              fontSize: 24,
+                              fontSize: 56,
                               color: AppColors.white,
-                              // fontWeight: FontWeight.w100,
+                              fontWeight: FontWeight.w200,
                             ),
                           ),
-                        ),
-                        SizedBox(width: 32.0),
-                        FaIcon(
-                          FontAwesomeIcons.rainbow,
-                          size: 80,
-                          color: AppColors.white,
-                        ),
-                      ],
-                    ),
-                    const Expanded(child: SizedBox(height: 10.0)),
-                    Column(
-                      children: [
-                        Text(
-                          'Lahore',
-                          style: TextStyle(
-                            fontSize: 14,
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(bottom: 28.0, left: 5.0),
+                            child: Text(
+                              '\u00B0',
+                              style: TextStyle(
+                                fontSize: 32,
+                                color: AppColors.white,
+                                fontWeight: FontWeight.w100,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 32.0),
+                          FaIcon(
+                            FontAwesomeIcons.rainbow,
+                            size: 80,
                             color: AppColors.white,
                           ),
-                        ),
-                        Text(
-                          'Haze',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: AppColors.white.withOpacity(0.5),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          // HumidityBar(currentHumidity: humidity.toDouble()),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                width: 140.0,
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      'Feel Like:',
-                                      style: TextStyle(
-                                          height: 0.0,
-                                          color:
-                                              AppColors.white.withOpacity(0.5),
-                                          fontSize: 12.0),
-                                    ),
-                                    SizedBox(width: 37.0),
-                                    Text(
-                                      '32\u00B0',
-                                      style: TextStyle(
-                                          height: 0.0,
-                                          color: AppColors.white,
-                                          fontSize: 12.0),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              SizedBox(height: 20.0),
-                              Container(
-                                width: 140.0,
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      'Wind Speed:',
-                                      style: TextStyle(
-                                          height: 0.0,
-                                          color:
-                                              AppColors.white.withOpacity(0.5),
-                                          fontSize: 12.0),
-                                    ),
-                                    SizedBox(width: 15.0),
-                                    Text(
-                                      '14 km',
-                                      style: TextStyle(
-                                          height: 0.0,
-                                          color: AppColors.white,
-                                          fontSize: 12.0),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          )
                         ],
                       ),
-                    ),
-                  ],
+                      const Expanded(child: SizedBox(height: 10.0)),
+                      Column(
+                        children: [
+                          Text(
+                            'Lahore',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppColors.white,
+                            ),
+                          ),
+                          Text(
+                            'Haze',
+                            style: TextStyle(
+                              color: AppColors.white.withOpacity(0.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            HumidityBar(currentHumidity: 67.4),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 140.0,
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        'Feel Like:',
+                                        style: TextStyle(
+                                          height: 0.0,
+                                          color:
+                                              AppColors.white.withOpacity(0.5),
+                                        ),
+                                      ),
+                                      SizedBox(width: 37.0),
+                                      Text(
+                                        '32\u00B0',
+                                        style: TextStyle(
+                                          height: 0.0,
+                                          color: AppColors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(height: 20.0),
+                                Container(
+                                  width: 140.0,
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        'Wind Speed:',
+                                        style: TextStyle(
+                                          height: 0.0,
+                                          color:
+                                              AppColors.white.withOpacity(0.5),
+                                        ),
+                                      ),
+                                      SizedBox(width: 15.0),
+                                      Text(
+                                        '14 km',
+                                        style: TextStyle(
+                                          height: 0.0,
+                                          color: AppColors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
